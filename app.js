242af9cc6e1958c40ac5d14e6260cd8d37d2a534
Rafael Sobral Coutinho <125751433+Rafael0106@users.noreply.github.com>
2025-01-15 17:43:37 -0300

    Contabilizando tentativas
    Adicionado a variável "tentativas" dentro do while para contabilizar as tentativas de chutes do usuário até chegar ao número secreto

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
-console.log(numeroSecreto)
-let chute 
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
 //para mostrar no consle qual foi o valor do chute escolhido pelo usuário
 console.log('Valor do chute:', chute);
@@ -13,13 +14,15 @@ while (chute != numeroSecreto){
     chute = prompt('Escolha um número entre 1 a 10');
 
     if (chute == numeroSecreto){
-        alert(`Isso aí! você descobriu o número secreto ${numeroSecreto}`);
+        alert(`Isso aí! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
     } else {
         if (chute > numeroSecreto){
             alert(`O número secreto é menor que ${chute}`);
         } else {
-            alert(`O número secreto é maior que ${chute}`)
+            alert(`O número secreto é maior que ${chute}`);
         }
+        // tentativas = tentativas + 1;
+        tentativas++;
     }
 }
 
