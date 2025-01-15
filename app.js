babfb9f83a4c4d519d31f8c71efd7f75e174b756
Rafael Sobral Coutinho <125751433+Rafael0106@users.noreply.github.com>
2025-01-15 16:52:19 -0300

    Concatenação
    A concatenação em JS é necessário o caractere "$", junto das chaves "{ }" e dentro das chaves vai a variável que você quer concatenar junto com o texto

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
+console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 a 10');
 
 if (chute == numeroSecreto){
-    console.log("Isso aí! você descobriu o número secreto (5)");
+    console.log(`Isso aí! você descobriu o número secreto ${numeroSecreto}`);
+} else {
+    alert('Você errou :(')
 }
