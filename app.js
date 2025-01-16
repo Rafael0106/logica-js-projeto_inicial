707d2fabfa9ff7b18dd5c36768c0060fd516fe7f
Rafael Sobral Coutinho <125751433+Rafael0106@users.noreply.github.com>
2025-01-16 20:00:33 -0300

    Função Math.random
    É necessária a multiplicação por 100, pois essa função irá reconhecer os algoritmos do 0 ao 99, portanto estamos falando para o programa colocar um número aleatório de 0 a 99 + 1, ou seja um número de 0 a 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 4;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
@@ -11,7 +11,7 @@ console.log('Valor do chute:', chute);
 console.log('Resultado da comparação:', chute == numeroSecreto);
 
 while (chute != numeroSecreto){
-    chute = prompt('Escolha um número entre 1 a 10');
+    chute = prompt('Escolha um número entre 1 a 100');
 
     if (chute == numeroSecreto){
         break;
